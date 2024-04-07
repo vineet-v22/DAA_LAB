eddc21eace4bc880266b90ffc70b5005e46b1e93
vineet-v22 <vermavineet418@gmail.com>
2024-04-07 22:35:58 +0530

    Create Q1.cpp

---
+++ Lab_8/Q1/Q1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define  ffl(i,a,b) for(int i=a;i<b;i++)
+#define ffu(i,a,b) for(int i=a;i<=b;i++)
+void solve(){
+    int n,m,l,b,c,d,e;
+    int a[n];
+    vector<int>v(n);
+}
+
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+return 0;
+}
